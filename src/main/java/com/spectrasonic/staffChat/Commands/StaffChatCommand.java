359ab620218e03f2b2e9841d01b73731e2228122
Manuel Heredia <Spectrasonic117@gmail.com>
2024-11-14 13:59:39 -0600

    import hex colors with bungee

--- src/main/java/com/spectrasonic/staffChat/Commands/StaffChatCommand.java
+++ src/main/java/com/spectrasonic/staffChat/Commands/StaffChatCommand.java
@@ -1,19 +1,19 @@
 package com.spectrasonic.staffChat.Commands;
 
 import com.spectrasonic.staffChat.Main;
+import net.md_5.bungee.api.ChatColor;
 import com.spectrasonic.staffChat.Utils.MessageUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.entity.Player;
-import org.bukkit.ChatColor;
 
 public class StaffChatCommand implements CommandExecutor {
 
     public StaffChatCommand(Main plugin) {
     }
-
+    @SuppressWarnings("all")
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player player) {
@@ -23,16 +23,18 @@ public class StaffChatCommand implements CommandExecutor {
                     for (String arg : args) {
                         message.append(arg).append(" ");
                     }
-                    String formattedMessage = ChatColor.translateAlternateColorCodes('&', "&7[&dStaff&7] &6» &b" + message.toString().trim());
+                    String SCPREFIX = ChatColor.translateAlternateColorCodes('&', "&7[&dStaff&7] &6» &b");
+                    String formattedMessage = SCPREFIX + message.toString();
                     Bukkit.getOnlinePlayers().stream()
                             .filter(p -> p.hasPermission("staffchat.chat"))
                             .forEach(p -> p.sendMessage(formattedMessage));
                 } else {
-                    MessageUtils.sendMessage(sender, "&e[❗] Usage: /sc <sessage>");
+                    player.sendMessage(net.md_5.bungee.api.ChatColor.of("#FFFF") + "[❗] Usage: /sc <sessage>");
 
                 }
             } else {
                 MessageUtils.sendMessage(sender, "&e[❗] You do not have permission to use this command.");
+                player.sendMessage(net.md_5.bungee.api.ChatColor.of("#FFFF") + "[❗] You do not have permission to use this command.");
             }
             return true;
         }
