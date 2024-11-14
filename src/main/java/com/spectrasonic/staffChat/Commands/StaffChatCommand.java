fe0802325737881351128861719cd346d1f2a17b
Manuel Heredia <Spectrasonic117@gmail.com>
2024-11-14 12:17:28 -0600

    🏁 - v1.0.0

---
+++ src/main/java/com/spectrasonic/staffChat/Commands/StaffChatCommand.java
@@ -0,0 +1,46 @@
+package com.spectrasonic.staffChat.Commands;
+
+import com.spectrasonic.staffChat.Main;
+import com.spectrasonic.staffChat.Utils.MessageUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.entity.Player;
+import org.bukkit.ChatColor;
+
+public class StaffChatCommand implements CommandExecutor {
+
+    public StaffChatCommand(Main plugin) {
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player player) {
+            if (player.hasPermission("staffchat.chat") || player.isOp()) {
+                if (args.length > 0) {
+                    StringBuilder message = new StringBuilder();
+                    for (String arg : args) {
+                        message.append(arg).append(" ");
+                    }
+                    // Send the message to staff
+                    String formattedMessage = ChatColor.translateAlternateColorCodes('&', "&7[&dStaff&7] &6» &b" + message.toString().trim());
+                    Bukkit.getOnlinePlayers().stream()
+                            .filter(p -> p.hasPermission("staffchat.chat"))
+                            .forEach(p -> p.sendMessage(formattedMessage));
+                } else {
+                    player.sendMessage(ChatColor.RED + "Usage: /sc <message>");
+                    MessageUtils.sendMessage(sender, "&e[❗] Usage: /sc <sessage>");
+
+                }
+            } else {
+                player.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
+                MessageUtils.sendMessage(sender, "&cYou do not have permission to use this command.");
+
+
+            }
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/main/java/com/spectrasonic/staffChat/Main.java
@@ -0,0 +1,27 @@
+package com.spectrasonic.staffChat;
+
+import com.spectrasonic.staffChat.Commands.StaffChatCommand;
+import com.spectrasonic.staffChat.Utils.MessageUtils;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        MessageUtils.sendStartupMessage(this);
+        registerCommands();
+    }
+
+    @Override
+    public void onDisable() {
+        MessageUtils.sendShutdownMessage(this);
+    }
+
+    public void registerCommands(){
+        Objects.requireNonNull(this.getCommand("sc")).setExecutor(new StaffChatCommand(this));
+    }
+
+}
